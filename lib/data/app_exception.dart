eae40217736fea53aa23af7338ee7bc84940f077
roy <goutomroy770@gmail.com>
2023-09-24 12:53:41 +0600

    App Exception initializing

--- lib/data/app_exception.dart
+++ lib/data/app_exception.dart
@@ -1,1 +1,24 @@
+class AppException implements Exception {
+  final _message;
+  final _prefix;
+  AppException([this._message, this._prefix]);
+  String toString() => '$_prefix$_message';
+}
 
+class FetchDataException extends AppException {
+  FetchDataException([String? message])
+      : super(message, 'Error During Communication');
+}
+
+class BadRequestException extends AppException {
+  BadRequestException([String? message]) : super(message, 'Invailid Request');
+}
+
+class UnAuthorisedException extends AppException {
+  UnAuthorisedException([String? message])
+      : super(message, 'Unauthorised Request');
+}
+
+class InvailidInputException extends AppException {
+  InvailidInputException([String? message]) : super(message, 'Invailid Input');
+}
